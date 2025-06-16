9dac9514d4515f483f92fbb4ebcbcbfbe506a69f
Candex22 <candemolinari20@gmail.com>
2025-06-16 19:55:41 -0300

    intento para subir a render

--- Server/main.js
+++ Server/main.js
@@ -69,9 +69,10 @@ app.use(express.urlencoded({ extended: true }));
 // });
 
 // Encender servidor
-app.listen(PORT, () => {
-    console.log(`PAGINA: localhost: ${PORT}`)
-})
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor corriendo en el puerto ${port}`);
+});
 
 
 const isLogged = (req, res, next) => {
